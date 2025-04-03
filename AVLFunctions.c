852336f41985167bfcaa37a2e6735bd7c6259fab
LiamSalazar <150866232+LiamSalazar@users.noreply.github.com>
2025-04-03 11:33:02 -0600

    Update AVLFunctions.c

--- AVLFunctions.c
+++ AVLFunctions.c
@@ -1,3 +1,4 @@
+#pragma once
 #include<stdio.h>
 #include<stdlib.h>
 #include".\Elem\ElemInt.h"
@@ -29,11 +30,22 @@ int factorBalanceo(DicBin a){
     return altura(izqAB(a)) - altura(derAB(a));
 }
 
+int abs(int x){
+    return (x >= 0)? x:-x; 
+}
+
+int esAVL(DicBin a){
+    if (abs(factorBalanceo(a))>1)
+        return 0;
+    else return 1;
+}
+
 DicBin balancear(DicBin a){
     if(esvacioAB(a)) return vacioAB();
     DicBin izq = balancear(izqAB(a)); // Balanceo arboles izquierdos
     DicBin der = balancear(derAB(a)); // Balanceo arboles derechos
     DicBin nuevo = consAB(raiz(a), izq, der); // Arbol con subs balanceados
+    
     int fb = factorBalanceo(nuevo);
     if(fb > 1){ // Mas a la izquierda
         if(factorBalanceo(izqAB(nuevo)) >= 0) // No zig zag porque más a la izquierda
@@ -48,5 +60,65 @@ DicBin balancear(DicBin a){
             return rotarIzquierda(consAB(raiz(nuevo), izqAB(nuevo), rotarDerecha(derAB(nuevo))));
     }
     return nuevo;
+    
 }
 
+DicBin hacerAVL(DicBin a){
+    while(!esAVL(a)){
+        a = balancear(a);
+    }
+    return a;
+}
+
+int esHoja(DicBin e){
+    if(izqAB(e) == NULL && derAB(e) == NULL) return 1;
+    return 0;
+}
+
+Elem maxIzq(DicBin a){
+    if(esHoja(a))
+        return raiz(a);
+    else if (esvacioAB(derAB(a)))
+        return raiz(a);
+    return maxIzq(derAB(a));
+}
+
+Elem minDer(DicBin a){
+    if(esHoja(a))
+        return raiz(a);
+    else if(!esvacioAB(izqAB(a)))
+        return minDer(izqAB(a));
+}
+
+DicBin eliminaMaximo(DicBin a){
+    if(esvacioAB(derAB(a)))
+        return NULL;
+    return consAB(raiz(a),izqAB(a),eliminaMaximo(derAB(a)));
+}
+
+DicBin eliminaMinimo(DicBin a){
+    if(esvacioAB(izqAB(a)))
+        return NULL;
+    return consAB(raiz(a), eliminaMinimo(izqAB(a)), derAB(a));
+}
+
+DicBin eliminaRaiz(DicBin a){
+    if(esHoja(a))
+        return vacioAB();
+    else if(esvacioAB(izqAB(a)))
+        return derAB(a);
+    else if(esvacioAB(derAB(a)))
+        return izqAB(a);    
+    return consAB(maxIzq(izqAB(a)), eliminaMaximo(izqAB(a)),derAB(a));
+}
+
+DicBin eliminaElemento(DicBin a, Elem e){
+    if(!esvacioAB(a))
+        return vacioAB();
+    else if(SonIguales(raiz(a), e))
+        return eliminaRaiz(a);
+    else if(EsMenor(raiz(a),e))
+        return consAB(raiz(a), izqAB(a),eliminaElemento(derAB(a), e));
+    return consAB(raiz(a), eliminaElemento(izqAB(a), e), derAB(a));
+    
+}
