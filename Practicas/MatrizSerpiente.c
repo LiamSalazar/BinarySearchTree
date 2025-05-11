48bab0b65355968d67bbfd8d798166c6ae43afe8
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2025-05-10 20:35:39 -0600

    Change

--- Practicas/MatrizSerpiente.c
+++ Practicas/MatrizSerpiente.c
@@ -4,30 +4,29 @@
 int main() {
     int matriz[N][N];
     int top = 0;
-    int bottom = N - 1;
+    int down = N - 1;
     int left = 0;
     int right = N - 1;
-    int count = 1;
+    int num = 1;
 
-    while (count <= N * N) {
-        for (int i = left; i <= right && count <= N * N; i++)
-            matriz[top][i] = count++;
+    while (num <= N * N) {
+        for (int i = left; i <= right && num <= N * N; i++)
+            matriz[top][i] = num++;
         top++;
 
-        for (int i = top; i <= bottom && count <= N * N; i++)
-            matriz[i][right] = count++;
+        for (int i = top; i <= down && num <= N * N; i++)
+            matriz[i][right] = num++;
         right--;
 
-        for (int i = right; i >= left && count <= N * N; i--)
-            matriz[bottom][i] = count++;
-        bottom--;
+        for (int i = right; i >= left && num <= N * N; i--)
+            matriz[down][i] = num++;
+        down--;
 
-        for (int i = bottom; i >= top && count <= N * N; i--)
-            matriz[i][left] = count++;
+        for (int i = down; i >= top && num <= N * N; i--)
+            matriz[i][left] = num++;
         left++;
     }
 
-    // Imprimir la matriz
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++)
             printf("%d\t ", matriz[i][j]);
