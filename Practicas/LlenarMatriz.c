bb6c1a250c1aa01b6495fba226c30dc760daee2f
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2025-05-22 08:24:56 -0600

    commit

--- Practicas/LlenarMatriz.c
+++ Practicas/LlenarMatriz.c
@@ -4,6 +4,7 @@ void llenar_matriz(int a[][100], int r, int c);
 void imprimir_matriz(int a[][100], int r, int c);
 void imprimir_renglon(int array[], int n);
 void burbuja(int a[], int n);
+void imprimir_matriz_ordenada_completa(int a[][100], int r, int c);
 
 int main(){
     int a[100][100];
@@ -14,7 +15,7 @@ int main(){
     puts("Ingrese c: ");
     scanf("%d", &c);
     llenar_matriz(a,r,c);
-    imprimir_matriz(a,r,c);
+    imprimir_matriz_ordenada_completa(a,r,c);
     return 0;
 }
 
@@ -51,6 +52,23 @@ void burbuja(int a[], int n){
 void imprimir_renglon(int array[], int c){
     burbuja(array, c);
     for(int i = 0; i < c; i++){
+        printf("%d\t", array[i]);
+    }
+}
+
+void imprimir_matriz_ordenada_completa(int a[][100], int r, int c){
+    puts("Impresión ordenada");
+    int size = r * c;
+    int array[100];
+    int contador = 0;
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            array[contador] = a[i][j];
+            contador++;
+        }
+    }
+    burbuja(array, size);
+    for(int i = 0; i < size; i++){
         printf("%d\t", array[i]);
     }
 }
