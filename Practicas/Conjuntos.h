d48097f33815cf0894651c59026343a779cdfa61
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2025-06-06 02:15:53 -0600

    commit

--- Practicas/Conjuntos.h
+++ Practicas/Conjuntos.h
@@ -8,10 +8,12 @@ Conjunto Vacio(){return vacia();}
 int EsVacio(Conjunto s){return s == Vacio();}
 
 int Contiene(Elem e, Conjunto s){
-    if(SonIguales(e, cabeza(s)) || EsVacio(s))
+    if(EsVacio(s))
+        return 0;
+    else if(SonIguales(e, cabeza(s)))
         return e == cabeza(s);
     else
-        Contiene(e, resto(s));
+        return Contiene(e, resto(s));
 }
 
 Conjunto Inserta(Elem e, Conjunto s){
@@ -25,7 +27,7 @@ Conjunto Elimina(Elem e, Conjunto  s){
     if(EsVacio(s))
         return s;
     else if(SonIguales(e, cabeza(s)))
-        return cons(e, resto(s));
+        return resto(s);
     else
         return cons(cabeza(s), Elimina(e, resto(s)));
 }
@@ -44,7 +46,7 @@ Conjunto Interseccion(Conjunto s1, Conjunto s2){
         Conjunto intersecta = Vacio();
         while(!EsVacio(s1)){
             if(Contiene(cabeza(s1), s2)){
-                Inserta(cabeza(s1),intersecta);
+                intersecta = Inserta(cabeza(s1),intersecta);
             }
             s1 = resto(s1);
         }
@@ -61,7 +63,7 @@ Conjunto Diferencia(Conjunto s1, Conjunto s2){
         Conjunto distintos = Vacio();
         while(!EsVacio(s1)){
             if(!Contiene(cabeza(s1), s2)){
-                Inserta(cabeza(s1),distintos);
+                distintos = Inserta(cabeza(s1),distintos);
             }
             s1 = resto(s1);
         }
@@ -76,4 +78,11 @@ int Cardinalidad(Conjunto s1){
         s1 = resto(s1);
     }
     return contador;
+}
+
+void ImpConjunto(Conjunto s){
+    if(!esvacia(s)){
+        ImpElem(cabeza(s));
+        ImpConjunto(resto(s));
+    }
 }
--- Practicas/PruebaConjuntos.c
+++ Practicas/PruebaConjuntos.c
@@ -2,5 +2,40 @@
 # include "Conjuntos.h"
 
 int main(){
-    
+    Conjunto c1 = Vacio();
+    c1 = Inserta(1,Inserta(2,Inserta(3,c1)));
+    puts("Conjunto 1:");
+    ImpConjunto(c1);
+    Conjunto c2 = Vacio();
+    c2 = Inserta(3,Inserta(3,Inserta(4,c2)));
+    puts("");
+    puts("");
+    puts("Conjunto 2:");
+    ImpConjunto(c2);
+    Conjunto c3 = Elimina(1,c1);
+    puts("");
+    puts("");
+    puts("Conjunto 1 eliminando 1:");
+    ImpConjunto(c3);
+    Conjunto c4 = Union(c1,c2);
+    puts("");
+    puts("");
+    puts("Conjunto Union:");
+    ImpConjunto(c4);
+    Conjunto c5 = Interseccion(c1,c2);
+    puts("");
+    puts("");
+    puts("Conjunto Interseccion:");
+    ImpConjunto(c5);
+    Conjunto c6 = Diferencia(c1,c2);
+    puts("");
+    puts("");
+    puts("Conjunto Diferencia:");
+    ImpConjunto(c6);
+    int cardinalidad = Cardinalidad(c1);
+    puts("");
+    puts("");
+    printf("Cardinalidad del conjunto 1: %d\n", cardinalidad);
+
+    return 0;
 }
