555515d7aba577b800742db3d82beab057707d3b
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2025-06-06 11:18:54 -0600

    commit

--- Practicas/SumarListas.c
+++ Practicas/SumarListas.c
@@ -1,62 +1,84 @@
-# include <stdio.h>
-# include "Lista.h"
-# include "ElemInt.h"
-# include "UsaLista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "Lista.h"
+#include "ElemInt.h"
+#include "UsaLista.h"
 
-int sizeList(Lista l){
+void imprimirLista(Lista l) {
+    while (!esvacia(l)) {
+        printf("%d", cabeza(l));
+        l = resto(l);
+    }
+    printf("\n");
+}
+
+int sizeList(Lista l) {
     int contador = 0;
-    while(!esvacia(l)){
+    while (!esvacia(l)) {
         contador++;
         l = resto(l);
     }
     return contador;
 }
 
-Lista rellenarLista(Lista l, int n){
-    for(int i = 0; i < n; i++){
-        l = cons(0,l);
-    }
-    return l;
+Lista rellenarLista(Lista l, int n) {
+    l = InvierteLista(l);
+    for (int i = 0; i < n; i++)
+        l = cons(0, l);
+    return InvierteLista(l);
 }
 
-Lista sumarListas(Lista a, Lista b){
+Lista sumarListas(Lista a, Lista b) {
     int acarreo = 0;
     int sizeA = sizeList(a);
     int sizeB = sizeList(b);
     Lista resultado = vacia();
-    if(sizeA > sizeB)
-        b = rellenarLista(b, sizeA-sizeB);
+
+    if (sizeA > sizeB)
+        b = rellenarLista(b, sizeA - sizeB);
     else
-        a = rellenarLista(a, sizeB-sizeA);
-    while(!esvacia(a)){
-        int sumaT = cabeza(a)+cabeza(b)+acarreo;
-        printf("\nSUMAElem: %d\n",sumaT);
-        if(sumaT < 10){
-            resultado = cons(sumaT, resultado);
-            acarreo = 0;
-        }else{
-            int unidad = sumaT-10;
-            resultado = cons(unidad, resultado);
-            acarreo = 1;
-        }
+        a = rellenarLista(a, sizeB - sizeA);
+
+    imprimirLista(a);
+    imprimirLista(b);
+    while (!esvacia(a)) {
+        int suma = cabeza(a) + cabeza(b) + acarreo;
+        resultado = cons(suma % 10, resultado);
+        acarreo = suma / 10;
         a = resto(a);
         b = resto(b);
     }
-    resultado = PegaListas(cons(acarreo,vacia()), resultado);
-    return resultado;
+    if (acarreo > 0)
+        resultado = cons(acarreo, resultado);
+
+    return InvierteLista(resultado);
 }
 
-int main(){
-    Lista a = vacia();
-    Lista b = vacia();
-    a = cons(1, cons(2, cons(3, a)));
-    b = cons(9, cons(9, b));
-    Lista r = sumarListas(a,b);
-    printf("\nNumero 1: \n");
-    ImpLista(a);
-    printf("\nNumero 2: \n");
-    ImpLista(b);
-    printf("\nSUMA: \n");
-    ImpLista(r);
+Lista ingresarNumero() {
+    Lista l = vacia();
+    char c;
+    printf("Ingrese los dígitos del número uno por uno:\n");
+    while (1) {
+        c = getchar();
+        if (c == '\n') break;
+        if (isdigit(c)) {
+            int digito = c - '0';
+            l = cons(digito, l); 
+        }
+    }
+    return l;
+}
+
+int main() {
+    printf("Número 1:\n");
+    Lista num1 = ingresarNumero();
+    printf("Número 2:\n");
+    Lista num2 = ingresarNumero();
+    Lista suma = sumarListas(num1, num2);
+    Lista sumaImpresion = InvierteLista(suma);
+    printf("Resultado de la suma: ");
+    imprimirLista(sumaImpresion);
+
     return 0;
 }
